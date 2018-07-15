a9bae729c7ae0c9cd2aec3b615a7432804c61df1
liuchenglong <18963337352@sina.cn>
2018-07-15 16:47:27 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import leftNav from '@/components/common/leftMenu'
+export default {
+  name: 'App',
+  components:{leftNav},
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div>
+	        <el-row >
+	            <el-col :span='7'>
+	            我是订单栏
+	            </el-col>
+	            <!--商品展示-->
+	            <el-col :span="17">
+	             我是产品栏
+	            </el-col>
+	        </el-row>
+	    </div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		name:'Pos',
+
+	}
+</script>
